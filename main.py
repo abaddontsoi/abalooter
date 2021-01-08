48808b1ad69aff79f43b1d7dc02048de28a892bc
abaddontsoi <styletsoi@gmail.com>
2021-01-08 20:44:33 +0800

    added a tip to remind user to install module 'instalooter' to make sure the program executable

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# the module "instalooter" must be installed, you may use "pip install instalooter" to install
+
+
 from instalooter.looters import * # import instalooter
 from login import * # login.py is a file storing ur login info
 import os # it works for folder operations
